00289e60d546755880784fc94d91c72528de7712
Ivan-Lanin <ivanandrlanin@gmail.com>
2023-04-30 17:35:39 +0200

    V0.02
    Enemy, enemy spawner, powerup, enemy waves

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private GameObject powerupIndicator;
     private GameObject focalPoint;
+    private bool hasPowerup = false;
+    private float powerupStrength = 15f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,34 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        if (forwardInput != 0) {
+            playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        }
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Powerup") && !hasPowerup) {
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup) {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayerDirection = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with " + collision.gameObject.name + "with powerup set to " + hasPowerup);
+            enemyRb.AddForce(awayFromPlayerDirection * powerupStrength, ForceMode.Impulse);
+        }
+    }
+
+    private IEnumerator PowerupCountdownRoutine() {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
     }
 }
